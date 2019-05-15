8471fcaa65be643006796a4d00cdc8b839741db4
Pål Grønås Drange <PGDR@equinor.com>
2019-05-15 14:11:16 +0200

    Add awesome datastructure forgetlist

---
+++ museumghosts/forgetlist.py
@@ -0,0 +1,64 @@
+import time
+import dataclasses
+
+
+class Forgetlist:
+    @dataclasses.dataclass(frozen=True)
+    class _Node:
+        payload: object
+        timestamp: float
+
+    def __init__(self, duration, clock=None, maxsize=2 ** 16):
+        self._duration = duration
+        self._idx = 0
+        self._lst = []
+        self.clock = time.time if clock is None else clock  # primarily for mocking
+        self.start = self.clock()
+        self.maxsize = maxsize
+
+    def now(self):
+        return round(self.clock() - self.start, 3)
+
+    def _valid(self, idx=None):
+        if idx is None:
+            idx = self._idx
+        now = self.now()
+        diff = now - (self._lst[idx].timestamp + self._duration)
+        return diff < 0
+
+    def __ffw(self):
+        if self._idx > self.maxsize:
+            self._lst = self._lst[self._idx :]
+            self._idx = 0
+        while self._idx < len(self._lst) and not self._valid():
+            self._idx += 1
+
+    def append(self, obj):
+        self._lst.append(Forgetlist._Node(obj, timestamp=self.now()))
+
+    def __iter__(self):
+        self.__ffw()
+        i = self._idx
+        while i < len(self._lst):
+            if self._valid(i):
+                yield self._lst[i].payload
+            i += 1
+
+    def __len__(self):
+        self.__ffw()
+        return len(self._lst) - self._idx
+
+    def __contains__(self, obj):
+        self.__ffw()
+        for elt in self:
+            if elt.payload == obj:
+                return True
+        return False
+
+    def __getitem__(self, idx):
+        self.__ffw()
+        return self._lst[self._idx + idx]
+
+    def __str__(self):
+        elts = ",".join([str(x) for x in self])
+        return f"<{self.now()} ⏱ [{elts}]>"
