a04efd11b9924342a786fccd79cc5bb475538bbb
Pål Grønås Drange <PGDR@equinor.com>
2019-05-21 18:24:23 +0200

    Add spotlight effect

--- museumghosts/graphics.py
+++ museumghosts/graphics.py
@@ -9,11 +9,28 @@ def draw_world(surface, world, speed, now):
     walls = world.walls
 
     surface.fill((0, 0, 0))
+    bg = pygame.image.load("floor.png")
+    bg_x, bg_y = bg.get_rect().size
+    for i in range(3):
+        for j in range(3):
+            surface.blit(bg, (bg_x * i, bg_y * j))
 
     for wall in walls:
         wall.draw(surface)
 
-    player.draw(surface, world=world, speed=speed)
+    vision_surface = pygame.Surface(world.size.tup)
+    vision_surface.fill((100, 100, 100))
+    vision_surface.set_alpha(100)
+
+    player.draw(vision_surface, world=world, speed=speed)
+
+    # invert vision polygon
+    pixels = pygame.surfarray.pixels2d(vision_surface)
+    pixels ^= 2 ** 32 - 1
+    del pixels
+
+    surface.blit(vision_surface, (0, 0), None, pygame.BLEND_RGB_SUB)
+
     for explosion in world.explosions:
         explosion.draw(surface, now)
 
